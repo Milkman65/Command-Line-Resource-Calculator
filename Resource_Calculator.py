bae566cda073e9c3718cc0cd8bc1aa5bdf7be0b8
Milkman65 <36807314+Milkman65@users.noreply.github.com>
2019-02-11 02:24:56 -0700

    Streamlining
    Streamlined code so it isn't a messy.
        Added a dictionary to store total resource values.
        Created multiple function to cut cut down on code.
        Added StoneStruct class to organize code better.

--- Resource_Calculator.py
+++ Resource_Calculator.py
@@ -1,19 +1,25 @@
 import time
+
 space = "    "
-stoneTotal = 0
-woodTotal = 0
-thatchTotal = 0
-def total():
+resource_dic = {"stone": 0, "wood": 0, "thatch": 0, "fungalwood": 0, "cp": 0, "fiber": 0}
+
+def total(): #prints total amount of resources needed
     print("|" * 70)
     print("Total Resources")
-    print("Stone: {}{}Wood: {}{}Thatch: {}".format(stoneTotal,space,woodTotal,space,thatchTotal))
+    for i in resource_dic:
+        if resource_dic[i] == 0:
+            return
+        else:
+            #mess with formating 
+            print(i, resource_dic[i])
     
 def mainMenu():
     print("x" * 70)    
     print("Stone: 1{}Wood: 2".format(space))
     print("Exit: 0")
     total() #prints total amount of resources
-    choice = input("Select a option: ")
+    choice = quickIn("select")
+    #error checking
     if choice.isdigit() == False:
         print("You must choose a number, Reloading")
         time.sleep(3)
@@ -22,60 +28,114 @@ def mainMenu():
         print("Input not in valid range, Reloading")
         time.sleep(3)
         mainMenu()
+    #menu system
     elif int(choice) == 0:
         print("Thanks for using me :)")
         time.sleep(3)
         exit()
     elif int(choice) == 1: #Stone
-        stone()
+        stoneMenu()
     elif int(choice) == 2: #Wood
         wood()
     else:
         print("All 'if' options exhausted, Reloading")
         time.sleep(3)
         mainMenu()
-        
-        
-def stone():
-    global stoneTotal
-    global woodTotal
-    global thatchTotal    
+               
+def stoneMenu():
+    global resource_dic   
     print("x" * 70)
-    print("Foundation: 1{}Wall: 2{}Cieling: 3".format(space,space))
+    print("Foundation: 1{}Wall: 2{}Ceiling: 3".format(space,space))
     print("back: 0")
     total() #prints total amount of resources
-    choice = input("Select a option: ")
+    choice = quickIn("select")
+    #error checking
     if choice.isdigit() == False:
         print("You must choose a number, Reloading")
         time.sleep(3)
-        stone()
+        stoneMenu()
     elif int(choice) > 3 or int(choice) < 0:
         print("Input not in valid range, Reloading")
         time.sleep(3)
-        stone()
+        stoneMenu()
+    #menu system
     elif int(choice) == 0: #back
         mainMenu()
     elif int(choice) == 1: #Stone Foundation
-        amount = input("How many do you want?: ")
-        amount = int(amount)
-        stoneTotal += 80 * amount
-        woodTotal += 40 * amount
-        thatchTotal += 30 * amount
-        stone()    
+        amount = int(quickIn(None))
+        StoneStruct.stoneFoundation(amount)
+        stoneMenu()
+    elif int(choice) == 2: #Stone Wall
+        amount = int(quickIn(None))
+        StoneStruct.stoneWall(amount)
+        stoneMenu()
+    elif int(choice) == 3: #Stone Ceiling
+        amount = int(quickIn(None))
+        StoneStruct.stoneCeiling(amount)
+        stoneMenu()  
     else:
         print("All 'if' options exhausted, Reloading")
         time.sleep(3)
-        stone()
-    
-def wood():
-    global stoneTotal
-    global woodTotal
-    global thatchTotal
+        stoneMenu()
+
+def quickIn(param): #makes coding in inputs less space consuming
+    if param ==  "select":
+        amount = input("Select a option: ")
+        return amount
+    else:
+        amount = input("How many do you want?: ")
+        return amount
+
+def woodMenu():
+    global resource_dic
     print("This is wood")
     
+class StoneStruct(object): #Class that calculates all the values of stone structs
+    def __init__(self, amount):
+        self.amount = amount
+        
+    def stoneFoundation(amount):
+        global resource_dic
+        resource_dic["stone"] += 80 * amount
+        resource_dic["wood"] += 40 * amount
+        resource_dic["thatch"] += 30 * amount
+        
+    def stoneWall(amount):
+        global resource_dic
+        resource_dic["stone"] += 40 * amount
+        resource_dic["wood"] += 20 * amount
+        resource_dic["thatch"] += 15 * amount
 
-    
+    def stoneCeiling(amount):
+        global resource_dic
+        resource_dic["stone"] += 60 * amount
+        resource_dic["wood"] += 30 * amount
+        resource_dic["thatch"] += 20 * amount
 
+    def stoneDoorframe(amount):
+        global resource_dic
+        resource_dic["stone"] += 30 * amount
+        resource_dic["wood"] += 16 * amount
+        resource_dic["thatch"] += 12 * amount
 
+    def stoneDinogate(amount):
+        global resource_dic
+        resource_dic["stone"] += 280 * amount
+        resource_dic["wood"] += 140 * amount
+        resource_dic["thatch"] += 100 * amount
+
+    def stoneCliffplatform(amount):
+        global resource_dic
+        resource_dic["stone"] += 1200 * amount
+        resource_dic["wood"] += 800 * amount
+        resource_dic["fiber"] += 600 * amount
+        resource_dic["cp"] += 250 * amount
+
+    def stoneStaircase(amount):
+        global resource_dic
+        resource_dic["stone"] += 200 * amount
+        resource_dic["wood"] += 100 * amount
+        resource_dic["thatch"] += 75 * amount
+        
 mainMenu()
     
